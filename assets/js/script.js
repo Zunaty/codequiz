09b14dbbddfff5763d26ac756fdb677d62071d6a
Victor Perez <zunaty@gmail.com>
2021-11-11 16:02:33 -0700

    Some html work, js questions array

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+var scores = [];
+var questions = [
+    {
+        name: "Question 1",
+        question: "Which of the following is used for an array?",
+        wrongAnswer1: "( )",
+        wrongAnswer2: "{ }",
+        wrongAnswer3: "< >",
+        correctAnswer: "[ ]",
+        viewed: "false"
+    },
+    {
+        name: "Question 2",
+        question: "Which of the following is used for an object?",
+        wrongAnswer1: "( )",
+        wrongAnswer2: "[ ]",
+        wrongAnswer3: "< >",
+        correctAnswer: "{ }",
+        viewed: "false"
+    },
+    {
+        name: "Question 3",
+        question: "Which of the following is used for html tags?",
+        wrongAnswer1: "( )",
+        wrongAnswer2: "{ }",
+        wrongAnswer3: "[ ]",
+        correctAnswer: "< >",
+        viewed: "false"
+    },
+    {
+        name: "Question 4",
+        question: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: "",
+        correctAnswer: "",
+        viewed: "false"
+    },
+    {
+        name: "Question 5",
+        question: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: "",
+        correctAnswer: "",
+        viewed: "false"
+    },
+];
+
+function resetGame() {
+
+};
+
+function startGame() {
+
+};
